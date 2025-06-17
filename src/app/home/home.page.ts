d7ab2f212c19efab1be4e7b358f4e1921ec6334a
Lucas Kazuo <kazuolucas1254@gmail.com>
2025-06-17 14:58:02 -0300

    listando pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from '../services/pokeapi.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import {   
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonList, 
+  IonItem, 
+  IonAvatar, 
+  IonLabel 
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonLabel,
+  ]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private pokeApi: PokeapiService, private router: Router) {}
+
+  ngOnInit() {
+    this.pokeApi.getPokemons(20, 0).subscribe(response => {
+      this.pokemons = response.results;
+    });
+  }
+
+  openDetails(name: string) {
+    this.router.navigate(['/details', name]);
+  }
+
+  getPokemonId(url: string): string {
+    const parts = url.split('/');
+    return parts[parts.length - 2];
+  }
+
 }
---
+++ src/app/services/pokeapi.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeapiService {
+  private baseUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) { }
+
+  // Método para listar Pokémons (com paginação)
+  getPokemons(limit: number = 20, offset: number = 0): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon?limit=${limit}&offset=${offset}`);
+  }
+
+  // Método para buscar detalhes de um Pokémon específico
+  getPokemonDetails(name: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`);
+  }
+}
